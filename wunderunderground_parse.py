88a7ece0f03e1e91d64bd1177e9337cea8388ece
dzeng <dzeng01@citymail.cuny.edu>
2017-04-05 20:22:15 -0400

    polished and cleaned up stuff

--- wunderunderground_parse.py
+++ wunderunderground_parse.py
@@ -2,20 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+start_year = 2015
+end_year = 2016
+filename = 'wunderground_{0}-{1}.csv'.format(start_year, end_year)
+months_with_30days = [4, 6, 9, 11]
+column_names = ['Time(EDT)', 'Temp.', 'Dew Point', 'Humidity', 'Pressure', 'Visibility', 'Wind Dir', 'Wind Speed',
+                'Gust Speed', 'Precip', 'Events', 'Conditions']
+header = ['TimeEDT', 'TemperatureF', 'Dew PointF', 'Humidity', 'Sea Level PressureIn', 'VisibilityMPH',
+          'Wind Direction', 'Wind SpeedMPH', 'Gust SpeedMPH', 'PrecipitationIn', 'Events', 'Conditions',
+          'WindDirDegrees', 'DateUTC']
 
-column_names = [
-'Time(EDT)',
-'Temp.',
-'Dew Point',
-'Humidity',
-'Pressure',
-'Visibility',
-'Wind Dir',
-'Wind Speed',
-'Gust Speed',
-'Precip',
-'Events',
-'Conditions',]
 
 def scrap_daily_wunder_html():
     # writer = csv.DictWriter(csvfile, fieldnames=column_names)
@@ -24,24 +20,25 @@ def scrap_daily_wunder_html():
     url = 'https://www.wunderground.com/history/airport/KNYC/2017/4/3/DailyHistory.html'
     r = requests.get(url)
     # soup = BeautifulSoup(r.content, 'html.parser')
-    soup = BeautifulSoup(r.content.decode('utf-8','ignore'),'html.parser')
-    table = soup.find('table',id="obsTable")
+    soup = BeautifulSoup(r.content.decode('utf-8', 'ignore'), 'html.parser')
+    table = soup.find('table', id="obsTable")
     rows = table.find_all('tr')[1:]
     for row in rows:
         columns = row.find_all('td')
         col_dict = {}
         for col_index, column in enumerate(columns):
-            col_dict[column_names[col_index]] = str(column.getText().strip()).replace('\xa0',' ')
+            col_dict[column_names[col_index]] = str(column.getText().strip()).replace('\xa0', ' ')
         # print(col_dict)
         writer.writerow(col_dict)
 
+
 def scrap_daily_wunder_csv(year, month, day):
     # writer = csv.writer(csvfile)
-    url = 'https://www.wunderground.com/history/airport/KNYC/{0}/{1}/{2}/DailyHistory.html?format=0'.format(year, month, day)
+    url = 'https://www.wunderground.com/history/airport/KNYC/{0}/{1}/{2}/DailyHistory.html?format=1'\
+                                                                                    .format(year, month, day)
     r = requests.get(url)
     contents = str(r.content).split("<br />\\n")
-    header = contents[0][4:].split(',')
-    # print(header)
+    # header = contents[0][4:].split(',')
     # writer.writerow(header)
     rows = [list(row.split(',')) for row in contents[1:]][:-1]
     for row in rows:
@@ -49,27 +46,25 @@ def scrap_daily_wunder_csv(year, month, day):
         writer.writerow(row)
 
 
-# scrap_daily_wunder_csv(2017,4,3)
-
-with open('wunderground_test.csv', 'w', newline='') as csvfile:
-    header = ['TimeEDT', 'TemperatureF', 'Dew PointF', 'Humidity', 'Sea Level PressureIn', 'VisibilityMPH', 'Wind Direction', 'Wind SpeedMPH', 'Gust SpeedMPH', 'PrecipitationIn', 'Events', 'Conditions', 'WindDirDegrees', 'DateUTC']
-    writer = csv.writer(csvfile)
-    # writer.writerow(header)
-
-    monthswith30days = [4,6,9,11]
-    year = 2016
-    if year%4 == 0:
-        leap_day = 29
-    else:
-        leap_day = 28
-
-    for month in range(1,13):
+def scrap_year(year):
+    # check if leap year
+    leap_day = 29 if year % 4 == 0 else 28
+    for month in range(1, 13):
         for day in range(1, 32):
+            # take care of feb
             if month == 2 and day > leap_day:
                 break
-            if month in monthswith30days and day > 30:
+            # take care of months with 30 days
+            if month in months_with_30days and day > 30:
                 break
-
             print(year, month, day)
             scrap_daily_wunder_csv(year, month, day)
 
+
+
+with open(filename , 'w', newline='') as csvfile:
+    # write header
+    writer = csv.writer(csvfile)
+
+    for year in range(start_year, end_year+1):
+        scrap_year(year)
